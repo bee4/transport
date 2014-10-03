e181884eddf42cea3d3b3dc421cc3b1e709cd721
Stéphane HULARD <s.hulard@chstudio.fr>
2014-10-03 16:28:37 +0200

    Nouvelle exception pour signifier les protocoles invalides
    Signed-off-by: Stéphane HULARD <s.hulard@chstudio.fr>

---
+++ src/Exception/UnknownProtocolException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the bee4/httpclient package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Bee4 2014
+ * @author	Stephane HULARD <s.hulard@chstudio.fr>
+ * @package Bee4\Transfer\Exception
+ */
+
+namespace Bee4\Transfer\Exception;
+
+/**
+ * Define cURL handle wrapper
+ * @package Bee4\Transfer\Exception
+ */
+class UnknownProtocolException extends \Exception {}
