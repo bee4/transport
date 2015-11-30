96e58a999cae2b599a6fd0f701a49252f13a3018
Stéphane HULARD <s.hulard@chstudio.fr>
2015-11-30 17:42:19 +0100

    Add context handlers for @Given and @Then

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -5,11 +5,18 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
+use Bee4\Transport\Client;
+
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context, SnippetAcceptingContext
 {
+    private $client;
+
+    private $request;
+    private $response;
+
     /**
      * Initializes context.
      *
@@ -17,7 +24,31 @@ class FeatureContext implements Context, SnippetAcceptingContext
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
      */
-    public function __construct()
+    public function __construct($rootUrl = "http://localhost")
     {
+        $this->client = new Client($rootUrl);
+        $this->response = null;
+        $this->request  = null;
+    }
+
+    /**
+     * @Given I make a :method request to :url with :body
+     * @Given I make a :method request to :url
+     */
+    public function iMadeARequestToTheUrl($method, $url, $body = null)
+    {
+        $this->request = $this->client->createRequest($method, $url);
+        if( isset($body) ) {
+            $this->request->setBody($body);
+        }
+        $this->response = $this->request->send();
+    }
+
+    /**
+     * @Then I should got a status of :status
+     */
+    public function iShouldGotAStatusOf($status)
+    {
+        return $this->response->getStatus()==$status;
     }
 }
