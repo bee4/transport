bf2d4c9f880df5ad6431c8af781ae9a4e94cc09b
Stéphane HULARD <s.hulard@chstudio.fr>
2015-12-09 17:28:32 +0100

    Add default implementation for Ssh GET & HEAD

---
+++ src/Message/Request/Ssh/Head.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the bee4/transport package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Bee4 2015
+ * @author  Stephane HULARD <s.hulard@chstudio.fr>
+ * @package Bee4\Transport\Message\Request\Ssh
+ */
+
+namespace Bee4\Transport\Message\Request\Ssh;
+
+/**
+ * SSH HEAD Request object
+ * @package Bee4\Transport\Message\Request\Ssh
+ */
+class Head extends SshRequest
+{
+    protected function prepare()
+    {
+        $this->addOption(CURLOPT_NOBODY, true);
+    }
+}
